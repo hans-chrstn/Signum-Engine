c8c43850abcd43d59c37448df6c966f63a189e36
hayato-oo <xuhiko13@gmail.com>
2026-09-22 02:54:07 -0400

    feat: add explicit native error

--- src/engine/core/error/engine_error.cpp
+++ src/engine/core/error/engine_error.cpp
@@ -4,7 +4,17 @@ namespace SNE::Engine::Core::Error {
     EngineError::EngineError(Code code, const char *msg)
         : std::runtime_error(msg), m_Code(code) {}
 
+    EngineError::EngineError(Code code, const char *msg,
+                             NativeError nativeError)
+        : std::runtime_error(msg), m_Code(code),
+          m_NativeError(std::move(nativeError)) {}
+
     Code EngineError::getCode() const noexcept {
         return m_Code;
+    }
+
+    const std::optional<NativeError> &
+    EngineError::getNativeError() const noexcept {
+        return m_NativeError;
     }
 } // namespace SNE::Engine::Core::Error
--- src/engine/core/error/engine_error.hpp
+++ src/engine/core/error/engine_error.hpp
@@ -4,17 +4,18 @@
 #include "native_error.hpp"
 #include <optional>
 #include <stdexcept>
-#include <string>
 
 namespace SNE::Engine::Core::Error {
     class EngineError : public std::runtime_error {
       private:
         Code m_Code;
-        NativeError m_NativeError;
+        std::optional<NativeError> m_NativeError;
 
       public:
         EngineError(Code code, const char *msg);
-        EngineError(Code code, const char *msg, NativeError);
+        EngineError(Code code, const char *msg, NativeError nativeError);
         [[nodiscard]] Code getCode() const noexcept;
+        [[nodiscard]] const std::optional<NativeError> &
+        getNativeError() const noexcept;
     };
 } // namespace SNE::Engine::Core::Error
